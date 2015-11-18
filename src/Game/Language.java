51c3017619a14c876107f78b5540f54d9d5709c1
Quaade94 <larsquaadeconnect@gmail.com>
2015-11-18 15:00:46 +0100

    Er blevet færdig med language klassen
    I kan nu bare skrive text ind i text filen i 13_CDIO_3 > bin >
    MessageBundle_en_GB.properties

--- src/Game/Language.java
+++ src/Game/Language.java
@@ -1,16 +1,30 @@
 package Game;
 
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class Language {
 	static ResourceBundle langs;
-	public static String[] getlang(String word) {
+	public static String getLang(String word)
+	{
 
-		if(langs==null){
+		if(langs==null)
+		{
 
 			setLocale(new Locale("en", "GB"));
 		}
 		return langs.getString(word);
-
+	}
+	@Override
+	public String toString() {
+		return "Current locale: " + langs.getLocale() + " on the following file: " + langs.getBaseBundleName() + " where " + langs.keySet().size() + "keys are contained";
+	}
+	public static void setLocale(Locale l)
+	{	
+		langs = ResourceBundle.getBundle("MessageBundle", l);
+	}
+	//Avoids creating objects of this class
+	private Language()
+	{
 	}	
 }
