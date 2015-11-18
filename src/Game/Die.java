db3d132c228b9e8f97021084d3ab1fccb65ef31f
Quaade94 <larsquaadeconnect@gmail.com>
2015-11-18 13:42:20 +0100

    Kopieret "Die" fra CDIO_2

--- src/Game/Die.java
+++ src/Game/Die.java
@@ -2,4 +2,22 @@ package Game;
 
 public class Die {
 
+	private int dice1;
+	private int dice2;
+	
+	public int[] roll() {
+
+		int [] roll = {0,0,0};
+
+			dice1 = (int) (Math.random() * 6) + 1;
+			dice2 = (int) (Math.random() * 6) + 1;
+
+			int sum = dice1 + dice2;
+			
+			roll [0] = dice1;
+			roll [1] = dice2;
+			roll [2] = sum;
+		
+		return roll;
+	}
 }
