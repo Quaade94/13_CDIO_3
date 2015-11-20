3d3f0fb34eabead7f60adbc59588d98dd0b3dfc7
Quaade94 <larsquaadeconnect@gmail.com>
2015-11-20 10:16:43 +0100

    Implementerer GUI og flytter MessageBundle fra bin til src

--- src/Game/Main.java
+++ src/Game/Main.java
@@ -1,13 +1,54 @@
 package Game;
 
-import Game.Language;
+import java.awt.Color;
+
+import desktop_codebehind.Car;
+import desktop_fields.Field;
+import desktop_fields.Shipping;
+import desktop_resources.GUI;
+import desktop_fields.Street;
+
+
 
 public class Main {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(Language.getLang("GNAME"));
+
+		//Spilleplade (SKAL være først)
+		Field[] fields = new Field[40];
+		GUI.create(fields);
 		
+		fields[0] = new Street.Builder()
+				.setTitle(Language.getLang("TER1"))
+				.setRent("")
+				.build();
+		
+		fields[1] = new Street.Builder()
+				.setTitle(Language.getLang(""))
+				.setRent("600,-")
+				.build();
+		
+		fields[0] = new Street.Builder().build();
+//		System.out.println(Language.getLang("TURN1"));
+	
+		
+
+//		Tilføjer en spiller til spillepladen
+		
+		Car car = new Car.Builder()
+				.typeUfo()
+				.patternHorizontalDualColor()
+				.primaryColor(Color.RED)
+				.secondaryColor(Color.BLUE)
+				.build();
+				GUI.addPlayer("John Rambo", 1000, car);
+		
+		
+				
+				
+		GUI.getUserButtonPressed("Press me", "ENTER");
+		GUI.close();
+
 	}
 
 }
