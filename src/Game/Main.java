6b23d3a2d6c653821b7ab21f7a5b217fd8dfb941
LukasBek <Lukas@Lukas-PC>
2015-11-22 03:42:53 +0100

    Oprettelse af navngivede spillere (ikke færdigt)

--- src/Game/Main.java
+++ src/Game/Main.java
@@ -20,63 +20,51 @@ public class Main {
 		
 		//Spilleplade (SKAL vÃ¦re fÃ¸rst)
 		new Fields();
-	
-
-//		Laver spilernes biler
 		
+		
+		
+		// Opretter en spiller ved at de indtaster deres navn
+		String player1Name = GUI.getUserString("Choose your name player 1");
+		//Laver spilernes biler
 		Car car1 = new Car.Builder()
 				.typeCar()
 				.patternHorizontalDualColor()
 				.primaryColor(Color.RED)
 				.secondaryColor(Color.BLUE)
 				.build();
+		GUI.addPlayer(player1Name, 1000, car1);
 		
+				
+		
+		//opretter spiller 2
+		String player2Name = GUI.getUserString("Choose your name player 2");
 		Car car2 = new Car.Builder()
 				.typeCar()
 				.patternHorizontalDualColor()
 				.primaryColor(Color.RED)
 				.secondaryColor(Color.BLUE)
 				.build();
+		GUI.addPlayer(player2Name, 1000, car2);
+				
 		
-		Car car3 = new Car.Builder()
-				.typeCar()
-				.patternHorizontalDualColor()
-				.primaryColor(Color.RED)
-				.secondaryColor(Color.BLUE)
-				.build();
-		
-		Car car4 = new Car.Builder()
-				.typeCar()
-				.patternHorizontalDualColor()
-				.primaryColor(Color.RED)
-				.secondaryColor(Color.BLUE)
-				.build();
-		
-		Car car5 = new Car.Builder()
-				.typeCar()
-				.patternHorizontalDualColor()
-				.primaryColor(Color.RED)
-				.secondaryColor(Color.BLUE)
-				.build();
-		
-		Car car6 = new Car.Builder()
-				.typeCar()
-				.patternHorizontalDualColor()
-				.primaryColor(Color.RED)
-				.secondaryColor(Color.BLUE)
-				.build();
+			
+//		Starten på et player check loop - der skal tjekke hvor mange spillere der skal være med. Laver det færdigt i morgen - Lukas22/11-2015
+		boolean newPlayer = GUI.getUserLeftButtonPressed("Er der flere spillere med?", "ja", "nej");
+		int i = 2;
+		if (newPlayer && i<=6){
+			String player3Name = GUI.getUserString("Choose your name player 3");
+			Car car3 = new Car.Builder().typeCar().patternHorizontalDualColor().primaryColor(Color.RED).secondaryColor(Color.BLUE).build();
+			GUI.addPlayer(player3Name, 3000, car3);
+			i++;
+		}
+		else if (!newPlayer){
+			System.out.println("Let the games. Begin.");
+			
+		}
 		
 		
-	
 		
-		
-//		TilfÃ¸jer spillere til spillepladen
-		
-		//Denne er der kun sÃ¥ GUI kan Ã¥bne
-		GUI.addPlayer("Name1", 1000, car1);
-		
-		
-//		if(playerAmount == 2){
+		//		if(playerAmount == 2){
 //			//input name here
 //			GUI.addPlayer("Name1", 1000, car1);
 //			//input name here
