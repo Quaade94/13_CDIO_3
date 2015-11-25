52d96715aefd3e4c256087f78183aa65d086a792
Quaade94 <larsquaadeconnect@gmail.com>
2015-11-25 14:33:28 +0100

    Ændrede player og main klasse mht. player position of navn

--- src/Game/Main.java
+++ src/Game/Main.java
@@ -1,4 +1,4 @@
-package Game;
+
 
 import java.awt.Color;
 import desktop_fields.Street;
@@ -7,6 +7,8 @@ import desktop_codebehind.Car;
 
 public class Main {
 
+	private int index;
+	
 	public static void main(String[] args) {
 
 		//Spilleplade (SKAL være først)
@@ -14,6 +16,9 @@ public class Main {
 		GameBoard.setGameBoard();
 		
 		Die Die = new Die();
+		PlayerPosition PlayerPosition = new PlayerPosition();
+		
+		Player Player = new Player();
 
 		PlayerAndCars PlayerAndCars = new PlayerAndCars((GUI.getUserInteger(Language.getLang("PLAYERNO"), 2, 6)));
 		PlayerAndCars.addPlayers();
@@ -21,15 +26,37 @@ public class Main {
 		//Start knappen
 		GUI.getUserButtonPressed(Language.getLang("RDY"), Language.getLang("SRT"));
 		
-		//player1 kaster med terningen
+		
+		//DETTE SKAL SLETTES
+		int antalspillere = 2;
+		
+		//player kaster med terningen og flytter brikken
+		//Der mangler at blive kaldt et navn og spillerens egen position
+		int turn = 0;
+		
+		while(antalspillere>1){
+			
 		GUI.getUserButtonPressed(Language.getLang("TURN"), Language.getLang("TD"));
 		Die.roll();
 		GUI.setDice(Die.getDice1(),Die.getDice2());
+		PlayerPosition.setPlayerPosition(0, Die.getDiceSum());
+		//placerer bilen på sin nye plads
 		
-		//While()
+		System.out.println(Player.getNameOfPlayer(0));
 		
+		GUI.setCar(PlayerPosition.getPlayerPosition(0), Player.getNameOfPlayer(0));
 		
-		//GUI.close();
+
+		
+		//fjerner bilen fra sin tidligere plads (med mindre det er første tur)
+		if (turn>0){
+		GUI.removeCar(PlayerPosition.getPlayerPosition(0)-Die.getDiceSum(), Player.getNameOfPlayer(0));
+		}
+		turn++;
+		}
+		
+		GUI.getUserButtonPressed(Language.getLang("WIN"), Language.getLang("CL"));
+		GUI.close();
 
 	}
 }
--- src/Game/PlayerPosition.java
+++ src/Game/PlayerPosition.java
@@ -1,9 +1,9 @@
-package Game;
+
 
 public class PlayerPosition {
 
 	Die Die = new Die();
-	
+
 	private int player1Position = 0;
 	private int player2Position = 0;
 	private int player3Position = 0;
@@ -11,13 +11,24 @@ public class PlayerPosition {
 	private int player5Position = 0;
 	private int player6Position = 0;
 	
-	public int[] playerPosittion(){
+	int[] playerPosition = {player1Position,player2Position,player3Position,player4Position,player5Position,player6Position,};
+
+
+	public int getPlayerPosition(int IDOfPlayer){
 		
-		int[] playerPosition = {player1Position,player2Position,player3Position,player4Position,player5Position,player6Position,};
+		return playerPosition[IDOfPlayer];
+
+	}
+
+	public void setPlayerPosition(int IDOfPlayer, int DiceSum){
+
 		
-		return playerPosition;
+		playerPosition[IDOfPlayer] = (playerPosition[IDOfPlayer] + DiceSum);
+//		System.out.println("set " + DiceSum);
+//		System.out.println(playerPosition[IDOfPlayer]);
 	}
 	
-	
-	
 }
+
+
+
