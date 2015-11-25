933be34bc808b604a446b32bfc209b65a48e74a0
magnusmisbat <magnusmisbat@gmail.com>
2015-11-25 20:53:31 +0100

    Tilføjet krop til landOnField, tilføjet getPrice metode i Territory
    Metoden returnere en pris på baggrund af den position spilleren har, der
    så er placeringen i arrayet den skal hente henholdsvis leve og pris.

--- src/Test/Territory.java
+++ src/Test/Territory.java
@@ -1,25 +1,38 @@
 package Test;
 
+
 public class Territory extends Ownable{
 
-	private int renrt;
-
+	
+	private int rent;
+	private int price;
+	
 	@Override
 	public void landOnField(int IDOfPlayer){
 		
+		rent = 0;
+		price = 0;
 		
+		int TempRent[] = GameBoard.getFieldRent();
+		int TempPrice[] = GameBoard.getFieldPrice();
 		
-		
-		
+		if (PlayerPosition.getPlayerPosition(IDOfPlayer) >= 0 && PlayerPosition.getPlayerPosition(IDOfPlayer) <= 10){
+			
+			rent = TempRent[PlayerPosition.getPlayerPosition(IDOfPlayer)];
+			price = TempPrice[PlayerPosition.getPlayerPosition(IDOfPlayer)];
+			
+		}
 	}
 	
 	@Override
 	public int getRent() {
-		return renrt;
-
+		return rent;
 		
 	}
 	
-	
-	
+	public int getPrice(){
+		
+		return price;
+		
+	}
 }
--- src/Test/TestKlasse.java
+++ src/Test/TestKlasse.java
@@ -1,5 +1,6 @@
 package Test;
 
+import Game.GameBoard;
 import Game.PlayerPosition;
 
 public abstract class TestKlasse {
@@ -7,6 +8,7 @@ public abstract class TestKlasse {
 	protected String Name;
 	
 	PlayerPosition PlayerPosition = new PlayerPosition();
+	GameBoard GameBoard = new GameBoard(21);
 
 	public abstract void landOnField(int IDOfPlayer);
 	
