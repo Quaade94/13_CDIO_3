e9987f1ac2389bcce2ff1fbb1960238cce7798bc
Big Fat Llama <lukas@10.16.99.208>
2015-11-25 14:43:50 +0100

    Tilføjet testpackage GameTests
    Også tilføjet JUnit test for Playeren og test af Dice

---
+++ src/GameTests/DieTest.java
@@ -0,0 +1,19 @@
+package GameTests;
+
+import Game.Die;
+
+public class DieTest {
+
+	public static void main(String[] args) {
+		
+		Die dice = new Die();
+		int i;
+		int roll1;
+		int roll2;
+		
+		for(i=0; i<5000; i++){
+			
+			roll1 = Die.getDice1();
+		}
+	}
+}
---
+++ src/GameTests/PlayerTest.java
@@ -0,0 +1,17 @@
+package GameTests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import Game.PlayerAndCars;
+import Game.Die;
+
+public class PlayerTest {
+
+//	Kan først laves når klasserne er færdige
+//	@Test
+//	public void testPlayer() {
+//		PlayerAndCars player = new PlayerAndCars(1);
+//		assertTrue(1000 == )
+//	}
+
+}
