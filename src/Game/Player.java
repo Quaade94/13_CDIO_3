1f7eb88aeda3c5e4fffd6e8471baf6debf5d20ac
Big Fat Llama <lukas@10.16.99.208>
2015-11-26 11:03:43 +0100

    Ændringer i Vehiclebuilder og PlayerPosition
    Har samlet begge klasser i en playerklasse, som har hele ansvaret for
    spilleren

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -4,10 +4,12 @@ import java.awt.Color;
 
 import desktop_codebehind.Car;
 import desktop_resources.GUI;
+import Game.VehicleBuilder;
 
-public class VehicleBuilder {
+public class Player{
+
+	Die Die = new Die();
 	
-//	Account Account = new Account(30000);
 	private String player1Name;
 	private String player2Name;
 	private String player3Name;
@@ -17,7 +19,7 @@ public class VehicleBuilder {
 	private int numOfPlayers;
 	String[] playerNameArray;
 	
-	public VehicleBuilder(int numOfPlayersConstructor) {
+	public void PlayerConstructor(int numOfPlayersConstructor){
 		numOfPlayers = numOfPlayersConstructor;
 		playerNameArray = new String[numOfPlayers];
 	}
@@ -35,22 +37,20 @@ public class VehicleBuilder {
 	private void addPlayerSwitch(int PlayerNumber){
 		
 		switch (PlayerNumber){
-		case 1: 		this.addPlayer1AndCar(); break;
-		case 2: 		this.addPlayer2AndCar(); break;
-		case 3: 		this.addPlayer3AndCar(); break;
-		case 4: 		this.addPlayer4AndCar(); break;
-		case 5: 		this.addPlayer5AndCar(); break;
-		case 6: 		this.addPlayer6AndCar(); break;
+		case 1: 		this.addPlayer1(); break;
+		case 2: 		this.addPlayer2(); break;
+		case 3: 		this.addPlayer3(); break;
+		case 4: 		this.addPlayer4(); break;
+		case 5: 		this.addPlayer5(); break;
+		case 6: 		this.addPlayer6(); break;
 		}
 	}
 	
 
 	
-	private void addPlayer1AndCar(){
+	private void addPlayer1(){
 		
-		player1Name = GUI.getUserString("Choose your name player 1");
-//		Account.addPlayerAndAccount(player1Name);
-		
+		player1Name = GUI.getUserString("Choose your name player 1");	
 		Car car1 = new Car.Builder()
 				.typeCar()
 				.patternHorizontalDualColor()
@@ -62,11 +62,9 @@ public class VehicleBuilder {
 		playerNameArray[0] = player1Name;
 	}
 	
-	private void addPlayer2AndCar(){
+	private void addPlayer2(){
 		
 		player2Name = GUI.getUserString("Choose your name player 2");
-//		Account.addPlayerAndAccount(player2Name);
-		
 		Car car2 = new Car.Builder()
 				.typeRacecar()
 				.patternHorizontalDualColor()
@@ -78,11 +76,9 @@ public class VehicleBuilder {
 		playerNameArray[1] = player2Name;
 	}
 	
-	private void addPlayer3AndCar(){
+	private void addPlayer3(){
 		
-		player3Name = GUI.getUserString("Choose your name player 3");
-//		Account.addPlayerAndAccount(player3Name);
-		
+		player3Name = GUI.getUserString("Choose your name player 3");		
 		Car car3 = new Car.Builder()
 				.typeTractor()
 				.patternHorizontalDualColor()
@@ -94,11 +90,8 @@ public class VehicleBuilder {
 		playerNameArray[2] = player3Name;
 	}
 	
-	private void addPlayer4AndCar(){
-		
-		player4Name = GUI.getUserString("Choose your name player 4");
-//		Account.addPlayerAndAccount(player4Name);
-		
+	private void addPlayer4(){		
+		player4Name = GUI.getUserString("Choose your name player 4");		
 		Car car4 = new Car.Builder()
 				.typeUfo()
 				.patternHorizontalDualColor()
@@ -110,7 +103,7 @@ public class VehicleBuilder {
 		playerNameArray[3] = player4Name;
 	}
 	
-	private void addPlayer5AndCar(){
+	private void addPlayer5(){
 		
 		player5Name = GUI.getUserString("Choose your name player 5");
 //		Account.addPlayerAndAccount(player5Name);
@@ -126,7 +119,7 @@ public class VehicleBuilder {
 		playerNameArray[4] = player5Name;
 	}
 	
-	private void addPlayer6AndCar(){
+	private void addPlayer6(){
 		
 		player6Name = GUI.getUserString("Choose your name player 6");
 //		Account.addPlayerAndAccount(player6Name);
@@ -152,4 +145,41 @@ public class VehicleBuilder {
 	public int getNumberOfPlayers(){
 		return playerNameArray.length;
 	}
+
+	
+	private int player1Position = 0;
+	private int player2Position = 0;
+	private int player3Position = 0;
+	private int player4Position = 0;
+	private int player5Position = 0;
+	private int player6Position = 0;
+	
+	private int fakePos = 0;
+	
+	public int carDestroyer = 0;
+	
+	int[] playerPosition = {player1Position,player2Position,player3Position,player4Position,player5Position,player6Position,};
+
+	public int getPlayerPosition(int IDOfPlayer){		
+		return playerPosition[IDOfPlayer];
+	}
+
+	public void setPlayerPosition(int IDOfPlayer, int DiceSum){
+		
+		playerPosition[IDOfPlayer] = (playerPosition[IDOfPlayer] + DiceSum);
+		
+		/**
+		 * For removing the car from the last known position
+		 **/
+		carDestroyer = playerPosition[IDOfPlayer]-DiceSum;
+		
+		/**
+		 * Makes the player go back to the start if he reaches the end of the GameBoard
+		 **/
+		if(playerPosition[IDOfPlayer]>21){
+			fakePos = playerPosition[IDOfPlayer];
+			fakePos = fakePos - 21;
+			playerPosition[IDOfPlayer] = fakePos;
+			}		
+	}	
 }
