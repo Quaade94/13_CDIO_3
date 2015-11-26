adce46b50c26547a1eeeca0d3ece84d507280d8e
magnusmisbat <magnusmisbat@gmail.com>
2015-11-26 09:42:41 +0100

    Forkortet og effektiviseret Territory

--- src/Test/Territory.java
+++ src/Test/Territory.java
@@ -1,26 +1,28 @@
 package Test;
 
+import Test.Ownable;
 
 public class Territory extends Ownable{
 
 	
 	private int rent;
 	private int price;
+	private int[] priceArray = {100,300,500,700,1000,1300,1600,2000,2600,3200};
+	private int [] rentArray = {1000,1500,2000,3000,4000,4300,4750,5000,5500};
+	
+	
 	
 	@Override
 	public void landOnField(int IDOfPlayer){
 		
 		rent = 0;
 		price = 0;
-		
-		int TempRent[] = GameBoard.getFieldRent();
-		int TempPrice[] = GameBoard.getFieldPrice();
-		
+
 		if (PlayerPosition.getPlayerPosition(IDOfPlayer) >= 0 && PlayerPosition.getPlayerPosition(IDOfPlayer) <= 10){
-			
-			rent = TempRent[PlayerPosition.getPlayerPosition(IDOfPlayer)];
-			price = TempPrice[PlayerPosition.getPlayerPosition(IDOfPlayer)];
-			
+						
+			price = priceArray[PlayerPosition.getPlayerPosition(IDOfPlayer)];
+			rent = rentArray[PlayerPosition.getPlayerPosition(IDOfPlayer)];
+
 		}
 	}
 	
@@ -29,7 +31,6 @@ public class Territory extends Ownable{
 		return rent;
 		
 	}
-	
 	public int getPrice(){
 		
 		return price;
