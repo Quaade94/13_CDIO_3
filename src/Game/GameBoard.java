3a7a2099be019db0883938edfaa8dccb9e337dd6
Quaade94 <larsquaadeconnect@gmail.com>
2015-11-26 11:18:15 +0100

    Ændrede priserne i GameBoard.

--- src/Game/GameBoard.java
+++ src/Game/GameBoard.java
@@ -2,6 +2,7 @@ package Game;
 
 import java.awt.Color;
 
+import Game.Language;
 import desktop_fields.Field;
 //import desktop_fields.Field;
 import desktop_fields.Street;
@@ -20,19 +21,11 @@ public class GameBoard {
 	private String[] fieldName = {"TER1","TER2","TER3","TER4","TER5","TER6","TER7","TER8","TER9","TER10","TER11","REF1","REF2","LAB1","LAB2","TAX1","TAX2","FLE1","FLE2", "FLE3", "FLE4"};
 	private String[] fieldDescription = {"TER_DES1","TER_DES1","TER_DES1","TER_DES1","TER_DES1","TER_DES1","TER_DES1","TER_DES1","TER_DES1","TER_DES1","TER_DES1","REF1_DES","REF2_DES","LAB_DES","LAB_DES","TAX1_DES","TAX2_DES","FLEET_DES","FLEET_DES","FLEET_DES","FLEET_DES"};
 	//private int[] fieldRent = {100,300,500,700,1000,1300,1600,2000,2600,3200,4000,-5000,-500,(Die.getDiceSum()*100),(Die.getDiceSum()*100),2000,4000,500,500,500,500};
-	//private int[] fieldPrice = {1000,1500,2000,3000,4000,4300,4750,5000,5500,6000,8000,0,0,2500,2500,0,0,4000,4000,4000,4000};
+	private int[] fieldPrice = {1000,1500,2000,3000,4000,4300,4750,5000,5500,6000,8000,5000,500,2500,2500,2000,4000,4000,4000,4000,4000};
 
 	public String[] getFieldNames(){
 		return fieldName;
 	}
-	
-//	public int[] getFieldRent(){
-//		return fieldRent;
-//		
-//	}
-//	public int[] getFieldPrice(){
-//		return fieldPrice;		
-//	}
 	
 	public void setGameBoard() {
 		Field[] fields = new Field[numberOfFields];	
@@ -45,7 +38,7 @@ public class GameBoard {
 				fields[index] = new Street.Builder()
 						.setTitle(Language.getLang(fieldName[index]))
 						.setDescription(Language.getLang(fieldDescription[index]))
-//						.setSubText(fieldRent[index] + ",-")
+						.setSubText(Language.getLang("REC")+fieldPrice[index] + ",-")
 //						.setRent(fieldRent[index] + ",-")
 						.setFgColor(Color.BLACK)
 						.setBgColor(Color.pink)
@@ -57,7 +50,7 @@ public class GameBoard {
 				fields[index] = new Street.Builder()
 						.setTitle(Language.getLang(fieldName[index]))
 						.setDescription(Language.getLang(fieldDescription[index]))
-//						.setSubText(fieldRent[index] + ",-")
+						.setSubText(Language.getLang("PAY")+fieldPrice[index] + ",-")
 //						.setRent(fieldRent[index] + ",-")
 						.setFgColor(Color.BLACK)
 						.setBgColor(Color.LIGHT_GRAY)
@@ -71,7 +64,7 @@ public class GameBoard {
 				fields[index] = new Street.Builder()
 						.setTitle(Language.getLang(fieldName[index]))
 						.setDescription(Language.getLang(fieldDescription[index]))
-//						.setSubText(fieldRent[index] + ",-")
+						.setSubText(Language.getLang("PRI")+fieldPrice[index] + ",-")
 //						.setRent(fieldRent[index] + ",-")
 						.setFgColor(Color.black)
 						.setBgColor(Color.cyan)
@@ -84,7 +77,7 @@ public class GameBoard {
 				fields[index] = new Street.Builder()
 						.setTitle(Language.getLang(fieldName[index]))
 						.setDescription(Language.getLang(fieldDescription[index]))
-//						.setSubText("Price: " +fieldPrice[index] + ",-")
+						.setSubText(Language.getLang("PRI")+fieldPrice[index] + ",-")
 //						.setRent(fieldRent[index] + ",-")
 						.setFgColor(Color.BLACK)
 						.setBgColor(Color.orange)
@@ -97,7 +90,7 @@ public class GameBoard {
 				fields[index] = new Street.Builder()
 						.setTitle(Language.getLang(fieldName[index]))
 						.setDescription(Language.getLang(fieldDescription[index]))
-//						.setSubText("Price: " +fieldPrice[index] + ",-")
+						.setSubText(Language.getLang("PRI")+fieldPrice[index] + ",-")
 //						.setRent(fieldRent[index] + ",-")
 						.setFgColor(Color.BLACK)
 						.setBgColor(Color.yellow)
