95a68401072b05e8c66197d976bcad3805add70c
William Wiberg <metronom_dk@hotmail.com>
2015-11-27 22:58:10 +0100

    Ryddet op i koden. Oversat kommentarer til engelsk. Fikset ligning for udregningen af game time.

--- src/Game/Main.java
+++ src/Game/Main.java
@@ -8,42 +8,39 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		long gameTime1 = System.currentTimeMillis();
-		
-		//Spilleplade (SKAL være først)
+		long gameTime1 = System.currentTimeMillis();		
+		// GameBoard must be created first
 		long responseTimeLaunch1 = System.currentTimeMillis();
 		GameBoard GameBoard = new GameBoard();
-//		GameBoard.toStrings();		toString metode til at udskrive arrayet.
+		//GameBoard.toStrings();		toString method to print the array containing the fieldsname
 		GameBoard.setGameBoard();
 		long responseTimeLaunch2 = System.currentTimeMillis();
 		System.out.println("Launch Time: "+(double)(responseTimeLaunch2-responseTimeLaunch1)/1000 + " seconds");
 		
-		//Importere
 		Die Die = new Die();
 		Player Player = new Player((GUI.getUserInteger(Language.getLang("PLAYERNO"), 2, 6)));
 		Account Account = new Account();
 		PlayerTurnSwitcher Turns1 = new PlayerTurnSwitcher();
-		
-
-		
+	
 		Turns1.setPlayerSize(Player.getNumberOfPlayers());
 		Player.addPlayers();
 		Account.addAccounts(Player.getNumberOfPlayers());
 		
-		//Start knappen
+		//Start game button
 		GUI.getUserButtonPressed(Language.getLang("RDY"), Language.getLang("SRT"));
-					
-		//Dette skal IKKE slettes
-		int[] FieldEffect = GameBoard.getFieldPrice();
 				
-		//player kaster med terningen og flytter brikke	
+		
+		
 		int antalSpillere = Player.getNumberOfPlayers();
 		System.out.println("Spillere " + antalSpillere); 
 		
 		while(antalSpillere>1){
 			if (Account.getPlayerStash(Turns1.getPlayerTurn()) >= 0){
 				
+				//Throw Dice button
 				GUI.getUserButtonPressed(Player.getPlayerName(Turns1.getPlayerTurn()) + Language.getLang("TURN"), Language.getLang("TD"));
+				
+				//player throws the dice and moves his car
 				Die.roll();
 				
 				GUI.setDice(Die.getDice1(),Die.getDice2());
@@ -52,7 +49,7 @@ public class Main {
 
 				GUI.setCar(Player.getPlayerPosition(Turns1.getPlayerTurn()), Player.getPlayerName(Turns1.getPlayerTurn()));
 				
-				//fjerner bilen fra sin tidligere plads (med mindre det er første tur)
+				// removes the car from its earlier position (unless it is the player's first turn)
 				if (Turns1.getIndependentTurn() > 0){
 					GUI.removeCar(Player.carDestroyer, Player.getPlayerName(Turns1.getPlayerTurn()));
 				}
@@ -88,20 +85,18 @@ public class Main {
 					Account.setPlayerStash(Turns1.getPlayerTurn(), -50000);
 					GUI.setBalance(Player.getPlayerName(Turns1.getPlayerTurn()), 0);
 				}
-			//fjerner bilen fra sin tidligere plads (med mindre det er første tur)
+
 				if (Turns1.getIndependentTurn() > 0){
 					GUI.removeCar(Player.carDestroyer, Player.getPlayerName(Turns1.getPlayerTurn()));
 				}
 				Turns1.scaleIndependentTurn();
 				Turns1.endTurn();
 			}else {
-//				Turns1.scaleIndependentTurn();
 				Account.setPlayerStash(Turns1.getPlayerTurn(), -50000);
 				GUI.setBalance(Player.getPlayerName(Turns1.getPlayerTurn()), 0);
 				Turns1.endTurn();
 			}
-				
-				
+							
 			}
 
 		if (antalSpillere == 1) {
@@ -110,8 +105,7 @@ public class Main {
 		
 		GUI.close();
 		long gameTime2 = System.currentTimeMillis();
-		double gameTime = (gameTime2-gameTime1/1000)/60;
-		System.out.println("Game lasted: " + (int)gameTime + " minutes");
-		
+		double gameTime = ((gameTime2-gameTime1)/1000)/60;
+		System.out.println("Game lasted: " + (int)gameTime + " minutes");		
 	}
 }
