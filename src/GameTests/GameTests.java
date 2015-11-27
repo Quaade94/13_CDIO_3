6d8ff87b6f63ea6efeba94511f8e789d0faa1960
Big Fat Llama <lukas@10.16.99.208>
2015-11-27 14:15:04 +0100

    Tilføjet test af spillerbeholdning

---
+++ src/GameTests/GameTests.java
@@ -0,0 +1,30 @@
+package GameTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import Game.Die;
+import Game.Main;
+import Game.GameBoard;
+import Game.Language;
+import Game.Player;
+
+public class GameTests {
+	
+	Die dice = new Die();
+	Player player = new Player();
+	GameBoard field = new GameBoard();
+	Language lang = new Language();
+	
+	@Test 
+	public void testPlayerPosition(){
+		
+		dice.roll();
+		int roll = dice.getDiceSum();
+		player.setPlayerPosition(0, roll);
+		
+		assertEquals(player.getPlayerPosition(0), roll);	
+	}
+
+}
