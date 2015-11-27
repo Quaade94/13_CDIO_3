fa27e95537e1b6f0451eceb3b2c4ebf6532427c6
William Wiberg <metronom_dk@hotmail.com>
2015-11-28 00:21:30 +0100

    Oversat kommentarer til engelsk i Language klassen.

--- src/Game/Language.java
+++ src/Game/Language.java
@@ -5,19 +5,21 @@ import java.util.ResourceBundle;
 
 public class Language {
 	
-	//Her Opretter vi en Resource Bundle med navnet 'text', den er static så den er bedre tilgænglig i andre klasser.
+	// Here, we create a Resource Bundle named 'text'. It is static so it is easier accessable in other classes.
 	static ResourceBundle text;
 	
 	public static void setLocale(Locale eng){	
 		
-		//Resource bundle 'text' bliver difineret som vores textfil som hedder 'MessageBundle..
-		//(som vi skriver vores sætningerne ind i).
-		//Messagebundle bliver altså kaldet her
+		/* Resource bundle 'text' gets defined as our textfile named 'MessageBundle..'
+		   where we write our strings. */
+		
+		
+		// So Messagebundle  gets called here
 		text = ResourceBundle.getBundle("MessageBundle", eng);
 		
 	}
 		
-	//en get-metode så vi kan kalde vores String til de andre klasser.
+	// a get method so we can call our string to the other klasses.
 	/**
 	 * 
 	 * @param keyword is a string that you define in capital letters.
@@ -25,8 +27,9 @@ public class Language {
 	 */
 	public static String getLang(String keyword){
 		
-		//Locale beskriver vores regions sprog, tegn osv..
-		//Kommandoen lyder Locale(String language, String country), her sætter vi den til typen engelsk i storbritanien.
+		// Locale describes our regions language, signs etc..
+		
+		// The command is Locale(String language, String country). Here we set it to the type english in great britain.
 		setLocale(new Locale("en", "GB"));
 		
 		return text.getString(keyword);
@@ -34,4 +37,4 @@ public class Language {
 	
 }
 
-//	.properties gør det muligt at åbne filen i alle text editors.
+//  .properties makes it possible to open the file in alle text editors.
