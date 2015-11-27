c658ff502ef1a9df57e6d97beb8dba4e67393cde
William Wiberg <metronom_dk@hotmail.com>
2015-11-27 23:55:54 +0100

    Slettet død kode i Account klassen

--- src/Game/Account.java
+++ src/Game/Account.java
@@ -1,7 +1,5 @@
 package Game;
 
-import java.util.Arrays;
-// Account er ikke en spiller, men en spiller har et account, saa ikke helt rigtigt, men kan virke alligevel
 
 public class Account {
 
@@ -26,60 +24,8 @@ public class Account {
 	public void setPlayerStash(int IDOfPlayer, int incMoney){
 		Accounts[IDOfPlayer] = Accounts[IDOfPlayer] + incMoney;
 	}
+	
 	public int[] getAccountArray(){
 		return Accounts;
-	}
-//	public static int getAccountSum(int IDOfPlayer){
-//		
-//		int SumOfAccount = 0;
-//		
-//		if (isValidID(IDOfPlayer)){
-//			if (isNotBroke(IDOfPlayer)){
-//				
-//				SumOfAccount = Accounts[IDOfPlayer];
-//			}
-//			if (!(isNotBroke(IDOfPlayer))){
-//				
-//				System.out.println("This Player ID = " + IDOfPlayer + " is broke, and cannot play anymore");
-//				
-//			}
-//			
-//		}
-//		if (!(isValidID(IDOfPlayer))){
-//			
-//			System.out.println("A fault happend - See; Game -> Account -> getAccountSum");
-//
-//		}
-//		
-//		return SumOfAccount;
-//
-//	}
-	
-//	public void addOrRemoveCapitalFromPlayerAccount(int IDOfPlayer, int AddRemove){
-//		
-//		if (super.isValidID(IDOfPlayer)){
-//			
-//			Accounts[IDOfPlayer] = Accounts[IDOfPlayer] + AddRemove;
-//		}
-//		if (!(super.isValidID(IDOfPlayer))){
-//			
-//			System.out.println("A fault happend - See; Game -> Account -> addOrRemoveCapitalFromPlayerAccount");
-//
-//		}
-//	}
-//	
-//	private static boolean isNotBroke(int IDOfPlayer){
-//		
-//		boolean broke = false;
-//		
-//		if (Account.getAccountSum(IDOfPlayer) < 0){
-//			
-//			broke = true;
-//			
-//		}
-//
-//		return broke;
-//		
-//	}
-	
+	}	
 }
