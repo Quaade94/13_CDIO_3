a0e40ecdc149aa8ea497bb690cdfd7a3539a03c8
Big Fat Llama <lukas@10.16.99.208>
2015-11-27 15:04:34 +0100

    Lavet en test af spillerens startpengebeholdning

--- src/GameTests/PlayerTest.java
+++ src/GameTests/PlayerTest.java
@@ -1,16 +1,24 @@
-package GameTests;
-
-import static org.junit.Assert.*;
-import org.junit.Test;
-import Game.Die;
-import Game.Player;
-
-public class PlayerTest {
-
-	
-	@Test
-	public void testPlayerCreation(){
-		
-		
-		}
-}
+//package GameTests;
+//
+//import static org.junit.Assert.*;
+//import org.junit.Test;
+//
+//import Game.Die;
+//import Game.Player;
+//import Game.Account;
+//
+//
+//public class PlayerTest {
+//
+////	Creates 3 players and their accounts
+//	Player player = new Player(3);
+//	Account account = new Account();
+//	
+//	@SuppressWarnings("deprecation")
+//	@Test
+//	public void testAccount(){
+//
+//		int player2Money = account.getPlayerStash(1);
+//		assertEquals(30000, player2Money);
+//	}
+//}
